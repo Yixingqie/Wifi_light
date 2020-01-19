44c29d6e17e499e335d0eed2279ed4fab41a37a7
Yixing Qie <yixingqie@gmail.com>
2020-01-19 15:37:58 -0500

    Add files via upload

---
+++ WiFi_light.lua
@@ -0,0 +1,44 @@
+--By Yixing Qie
+wifi.setmode(wifi.STATIONAP);
+wifi.ap.config({ssid="test",pwd="12345678"}); --set password and ssid
+--below sets a static ip address, incase the router resets
+wifi.sta.setip({ip="192.168.1.14",netmask="255.255.255.0",gateway="192.168.1.1"})
+
+--setting up pins on ESP8266
+led1 = 3
+led2 = 4
+gpio.mode(led1, gpio.OUTPUT)
+gpio.mode(led2, gpio.OUTPUT)
+srv=net.createServer(net.TCP)
+srv:listen(80,function(conn)
+    conn:on("receive", function(client,request)
+        local buf = "";
+        local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
+        if(method == nil)then
+            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
+        end
+        local _GET = {}
+        if (vars ~= nil)then
+            for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
+                _GET[k] = v
+            end
+        end
+        --Setting up the physical layout of site
+        buf = buf.."<h1> Wi-Fi light</h1>";
+        buf = buf.."<p>GPIO0 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
+        buf = buf.."<p>GPIO2 <a href=\"?pin=ON2\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF2\"><button>OFF</button></a></p>";
+        local _on,_off = "",""
+        if(_GET.pin == "ON1")then
+              gpio.write(led1, gpio.HIGH);
+        elseif(_GET.pin == "OFF1")then
+              gpio.write(led1, gpio.LOW);
+        elseif(_GET.pin == "ON2")then
+              gpio.write(led2, gpio.HIGH);
+        elseif(_GET.pin == "OFF2")then
+              gpio.write(led2, gpio.LOW);
+        end
+        client:send(buf);
+        client:close();
+        collectgarbage();
+    end)
+end)
